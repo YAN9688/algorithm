33097f2ba9ac5b68c0b8281ed16e9efed05170bf
YAN9688 <37128010+YAN9688@users.noreply.github.com>
2023-08-08 15:44:04 +0900

    Create 최빈값구하기.java

---
+++ SWEA/D2/최빈값구하기.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int num = sc.nextInt();
+			int[] arr = new int[101];
+			for (int i = 0; i < 1000; i++) {
+				arr[sc.nextInt()]++;
+			}
+			int max = 0;
+			int max_i = 0;
+			for (int i = 100; i >= 0; i--) {
+				if (arr[i] > max) {
+					max = arr[i];
+					max_i = i;
+				}
+			}
+			System.out.println("#" + num + " " + max_i);
+		}
+	}
+}
