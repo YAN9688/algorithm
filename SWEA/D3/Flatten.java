1d1993992f976b681737c51e86718b97630f677a
YAN9688 <37128010+YAN9688@users.noreply.github.com>
2023-08-08 15:43:35 +0900

    Update and rename SWEA/Flatten.java to SWEA/D3/Flatten.java

--- SWEA/D3/Flatten.java
+++ SWEA/D3/Flatten.java
@@ -6,21 +6,21 @@ class Solution {
 		Scanner sc = new Scanner(System.in);
 		for (int test_case = 1; test_case <= 10; test_case++) {
 			int[] a = new int[100];
-			int m= sc.nextInt();
-			int d =-1;
-			for(int i=0;i<100;i++) {
-				a[i]=sc.nextInt();
+			int m = sc.nextInt();
+			int d = -1;
+			for (int i = 0; i < 100; i++) {
+				a[i] = sc.nextInt();
 			}
 			Arrays.sort(a);
-			while(m>0) {
-				if(a[99]-a[0]<=1)
+			while (m > 0) {
+				if (a[99] - a[0] <= 1)
 					break;
-				m --;
+				m--;
 				a[99]--;
 				a[0]++;
 				Arrays.sort(a);
 			}
-			d= a[99]-a[0];
+			d = a[99] - a[0];
 			System.out.println("#" + test_case + " " + d);
 		}
 	}
