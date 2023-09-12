8fc4d83f0eea3ee0d7ee0b4377a2809211f2bb29
YAN9688 <37128010+YAN9688@users.noreply.github.com>
2023-09-12 15:52:13 +0900

    [level 0] Title: 옹알이 (1), Time: 0.22 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -3,13 +3,15 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         String[] bab = {"aya","ye","woo","ma"};
+        
         for(int i=0; i< babbling.length;i++){
+            
             for(int j=0;j<4;j++){
                babbling[i]=babbling[i].replace(bab[j],"_");
-            }
+              }
+            
             babbling[i]=babbling[i].replace("_","");
-            if (babbling[i].length() ==0)
-                answer++;
+            if(babbling[i].isEmpty()) answer++;
             }
         return answer;
     }
