d5eb8ca953a99b9bf8e4afbb93f0191e882731d8
YAN9688 <37128010+YAN9688@users.noreply.github.com>
2023-09-18 15:04:27 +0900

    Update

--- SWEA/D3/6057. 그래프의 삼각형/그래프의 삼각형.java
+++ SWEA/D3/6057. 그래프의 삼각형/그래프의 삼각형.java
@@ -1,21 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 class Solution {
 
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T = sc.nextInt();
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
 		for (int tc = 1; tc <= T; tc++) {
-			int N = sc.nextInt();
-			int M = sc.nextInt();
+			String[] NM = br.readLine().split(" ");
+			int N = Integer.parseInt(NM[0]);
+			int M = Integer.parseInt(NM[1]);
 			int answer = 0;
 			int[][] graph = new int[N][N];
+
 			for (int i = 0; i < M; i++) {
-				graph[sc.nextInt() - 1][sc.nextInt() - 1] = 1;
+				String[] edgeData = br.readLine().split(" ");
+				graph[Integer.parseInt(edgeData[0]) - 1][Integer.parseInt(edgeData[1]) - 1] = 1;
 			}
+
 			for (int i = 0; i < N; i++) {
 				List<Integer> list = new ArrayList<>();
 				for (int j = i + 1; j < N; j++) {
@@ -23,6 +30,7 @@ class Solution {
 						list.add(j);
 				}
 				int l = list.size();
+
 				if (l >= 2) {
 					for (int p1 = 0; p1 < l - 1; p1++) {
 						for (int p2 = p1 + 1; p2 < l; p2++) {
@@ -32,7 +40,8 @@ class Solution {
 					}
 				}
 			}
-			System.out.println("#" + tc + " " + answer);
+			sb.append("#").append(tc).append(" ").append(answer).append("\n");
 		}
+		System.out.print(sb.toString());
 	}
 }
