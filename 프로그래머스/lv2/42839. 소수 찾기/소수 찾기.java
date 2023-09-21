cd59e251a30360722f27f0a863e9eb2f8dcb9fe4
YAN9688 <37128010+YAN9688@users.noreply.github.com>
2023-09-21 16:59:09 +0900

    [level 2] Title: 소수 찾기, Time: 13.81 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -1,18 +1,20 @@
 import java.util.*;
-
-
 class Solution {
-    //같은 숫자가 적힌 조각 제거 > set
+    //같은 숫자가 적힌 조각과 첫자리가 0인 케이스 제거하기 위해 set 생성
 	public static HashSet<Integer> set;
 	public static boolean[] visited;
-    //소수 체크, num이 3보다 큰 조건에만 들어옴, 연산 최소화를위해 i*i <= num
+    // 소수 체크 메서드 
+    // 순열 조건에서 num>=2 일 때만 들어옴, 연산 최소화를위해 i가 2일때 한번 체크해주고, i가 짝수일땐 확인해줄 필요가 없으므로 i+2, i*i <= num
 	public static boolean check(int num) {
-		for (int i = 2; i*i <= num; i++) {
+        if (num==2) return true;
+        if (num % 2 == 0) return false;
+		for (int i = 3; i*i <= num; i+=2) {
 			if (num % i == 0) return false;
         }
 		return true;
 	} 
-    //순열 메서드
+    //순열 구현 메서드
+    //배열 안쓰고 그냥 빈 문자열을 만들고 이어붙이는 방식으로 했음
 	public static void per(String nu, String numbers, int len) {
 		if (nu.length() == len) {
 			int num = Integer.parseInt(nu);
@@ -30,11 +32,11 @@ class Solution {
 	}
         //구현부
 	public int solution(String numbers) {
-
+        //초기화
 		set=new HashSet<>();
+        visited = new boolean[numbers.length()];
         //고민했던부분: 원래 부분조합을 먼저 만들고 순서를 배열해줄지, 아래방법처럼할지 고민했는데 후자가 간단한 것 같아서 이렇게 함
 		for (int len = 1; len <= numbers.length(); len++) {
-			visited = new boolean[numbers.length()];
 			per("", numbers, len);
 		}
 		return set.size();
