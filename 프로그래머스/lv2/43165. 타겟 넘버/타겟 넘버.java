138a663f36c1960eaa4164e65c89d6a23b078f47
YAN9688 <37128010+YAN9688@users.noreply.github.com>
2023-09-25 16:39:54 +0900

    [level 2] Title: 타겟 넘버, Time: 0.45 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,21 +2,21 @@ class Solution {
 	static boolean[] visited;
 	static int answer;
 	static int n;
-    static int[] nums;
+
 	public int solution(int[] numbers, int target) {
 		answer = 0;
 		n = numbers.length;
 		visited = new boolean[n];
 		int sum = 0;
 		for (int i = 0; i < n; i++) {
-			sum = sum+ numbers[i];
+			sum = sum + numbers[i];
 		}
-        nums = numbers;
 		int diff = sum - target;
-		sel(0, diff);
+		sel(numbers, 0, diff);
 		return answer;
 	}
-	static void sel(int idx,int diff) {
+
+	static void sel(int[] numbers, int idx, int diff) {
 		if (diff < 0)
 			return;
 		else if (diff == 0) {
@@ -26,7 +26,7 @@ class Solution {
 		for (int i = idx; i < n; i++) {
 			if (!visited[i]) {
 				visited[i] = true;
-				sel( i , diff - (nums[i] * 2));
+				sel(numbers, i + 1, diff - (numbers[i] * 2));
 				visited[i] = false;
 			}
 		}
