27d771ee4a4b382d0f51f15308f6ff2e7b2cea6c
YAN9688 <37128010+YAN9688@users.noreply.github.com>
2023-10-31 09:43:00 +0900

    [Silver III] Title: N과 M (8), Time: 268 ms, Memory: 25548 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int n, m;
+	static StringBuilder sb;
+//	static boolean[] sel;
+	static int[] arr;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		sb = new StringBuilder();
+//		sel = new boolean[n];
+		comb(0, new int[m], 0);
+
+		System.out.println(sb.toString());
+	}
+
+	static void comb(int len, int[] ord, int idx) {
+		if (len == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(arr[ord[i]] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = idx; i < n; i++) {
+//			if (sel[i])
+//				continue;
+//			sel[i] = true;
+			ord[len] = i;
+			comb(len + 1, ord, i);
+//			sel[i] = false;
+		}
+	}
+}
