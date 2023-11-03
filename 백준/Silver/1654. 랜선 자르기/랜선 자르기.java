2d53fdd41d8aa76a048d228ce3ddab39cbd395fc
YAN9688 <37128010+YAN9688@users.noreply.github.com>
2023-11-03 10:13:37 +0900

    [Silver II] Title: 랜선 자르기, Time: 204 ms, Memory: 17332 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,35 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int k = sc.nextInt();
-		int target = sc.nextInt();
-		int[] arr = new int[k];
-		for (int i = 0; i < k; i++) {
-			int tmp = sc.nextInt();
-			arr[i] = tmp;
-		}
-		long x_min = 1;
-		long x_max = Arrays.stream(arr).max().getAsInt();
-		long find_x = 0;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		while (x_min <= x_max) {
-			long num = 0;
-			long x = (x_min + x_max) / 2;
-			for (int i = 0; i < k; i++) {
-				num += arr[i] / x;
-			}
-			if (num >= target) {
-				find_x = x;
-				x_min = x + 1;
-			} else {
-				x_max = x - 1;
-			}
-		}
+        String[] input = br.readLine().split(" ");
+        int k = Integer.parseInt(input[0]);
+        int target = Integer.parseInt(input[1]);
+        int[] arr = new int[k];
 
-		System.out.println(find_x);
+        for (int i = 0; i < k; i++) {
+            arr[i] = Integer.parseInt(br.readLine().trim());
+        }
 
-	}
+        long x_min = 1;
+        long x_max = Arrays.stream(arr).max().getAsInt();
+        long find_x = 0;
+
+        while (x_min <= x_max) {
+            long num = 0;
+            long x = (x_min + x_max) / 2;
+            for (int i = 0; i < k; i++) {
+                num += arr[i] / x;
+            }
+            if (num >= target) {
+                find_x = x;
+                x_min = x + 1;
+            } else {
+                x_max = x - 1;
+            }
+        }
+
+        bw.write(find_x + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
