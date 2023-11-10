cc2447e990830986920f0d9f91d2bf80f94fbf99
YAN9688 <37128010+YAN9688@users.noreply.github.com>
2023-11-10 15:41:24 +0900

    [Gold IV] Title: 거짓말, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,49 +1,51 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		int nums = sc.nextInt();
-		HashSet<Integer> set = new HashSet<>();
-		for (int i = 0; i < nums; i++) {
-			set.add(sc.nextInt());
-		}
-		List<Integer>[] party = new List[m];
-		for (int i = 0; i < m; i++) {
-			party[i] = new ArrayList<>();
-			int tmp = sc.nextInt();
-			for (int j = 0; j < tmp; j++) {
-				party[i].add(sc.nextInt());
-			}
-		}
-		boolean[] telling = new boolean[m];
-		for (int x = 0; x < m; x++) {
-			for (int i = 0; i < m; i++) {
-				if (telling[i])
-					continue;
-				boolean flag = false;
-				for (int j = 0; j < party[i].size(); j++) {
-					if (set.contains(party[i].get(j))) {
-						flag = true;
-						break;
-					}
-				}
-				if (flag) {
-					telling[i] = true;
-					for (int j = 0; j < party[i].size(); j++) {
-						set.add(party[i].get(j));
-					}
-				}
-			}
-		}
-		int cnt = 0;
-		for (int i = 0; i < m; i++) {
-			if (!telling[i])
-				cnt++;
-		}
-		System.out.println(cnt);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int nums = Integer.parseInt(st.nextToken());
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        List<Integer>[] party = new ArrayList[m];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            party[i] = new ArrayList<>();
+            int tmp = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < tmp; j++) {
+                party[i].add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        int cnt = m;
+        boolean[] telling = new boolean[m];
+        for (int x = 0; x < m; x++) {
+            for (int i = 0; i < m; i++) {
+                if (telling[i]) continue;
+                boolean flag = false;
+                for (int j = 0; j < party[i].size(); j++) {
+                    if (set.contains(party[i].get(j))) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag) {
+                    telling[i] = true;
+                    cnt--;
+                    for (int j = 0; j < party[i].size(); j++) {
+                        set.add(party[i].get(j));
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
 }
